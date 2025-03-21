07596a5f67934cbd8e220de188afa3fa49b619a6
misteryag <aingthierry@gmail.com>
2025-03-21 11:13:09 +0100

    Improved contact form and added map, pokeapi is now fully functionnal with css, fixed width issue with body

--- JS/pokemon.js
+++ JS/pokemon.js
@@ -1,8 +1,16 @@
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/bellsprout";
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+const card = document.getElementById("pokemon-card");
 
-async function fetchPokemonData() {
+const button = document.getElementById("search");
+
+button.addEventListener("click", () => {
+    const userInput = document.getElementById("text").value;
+    fetchPokemonData(userInput);
+});
+
+async function fetchPokemonData(userInput) {
     try{
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl+userInput);
 
         if (!response.ok){
             throw new Error(`Erreur HTTPS: ${response.status}`);
@@ -15,14 +23,12 @@ async function fetchPokemonData() {
         /* console.log(pokemonData); */
     }catch (error) {
         console.error('Erreur lors de la récupération:', error);
+        card.innerHTML =
+        `<p class="error">Erreur lors de la récupération:', ${error}</p>`;
     }
 }
 
-fetchPokemonData();
-
 function displayPokemon(pokemon){
-    const card = document.getElementById("pokemon-card");
-
     console.log(pokemon.name);
 
     card.innerHTML = 
@@ -35,21 +41,23 @@ function displayPokemon(pokemon){
             <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}"/>
         </div>
 
-        <div>
+        <div class="pc-t">
+            <h5>Type(s):</h5>
             ${pokemon.types.map(t => 
                 `<span>${t.type.name}</span>`
-            ).join('')}
+            ).join(' ')}
         </div>
 
         <div>
-            <span>Taille / Poids</span>
+            <h5>Height / Weight</h5>
             <p>${pokemon.height / 10} M / ${pokemon.weight / 10} KG</p>
         </div>
 
-        <div>
+        <div class="pc-a">
+            <h5>Abilities:</h5>
             ${pokemon.abilities.map(a => 
                 `<span>${a.ability.name}</span>`
-            ).join('')}
+            ).join(' ')}
         </div>
     </div>
     `;
---
+++ README.md
@@ -0,0 +1,17 @@
+# Introduction
+Ce site à été mit en ligne grâce à [GitHub Pages](https://pages.github.com/).
+
+Vous pouvez d'ailleurs accéder au site [ici](https://misteryag.github.io/TP0-HTML-BTS-SIO/).
+
+Ce site a pour but d'exploiter les bases du [HTML](https://fr.wikipedia.org/wiki/Hypertext_Markup_Language), [CSS](https://fr.wikipedia.org/wiki/Feuilles_de_style_en_cascade) et du [JS](https://fr.wikipedia.org/wiki/JavaScript) sur le thème d'un portfolio.
+
+# Présentation
+
+Le site contient 2 pages. La première contenant l'ensemble principal du site comme une carte de profile, le CV, l'indication des services et un formulaire de contact fonctionnel.
+
+La seconde page contient un mini formulaire pour faire une requête sur [PokéAPI](https://pokeapi.co/), une [API](https://fr.wikipedia.org/wiki/API) permettant de rechercher des informations sur le pokémon que l'on veut comme un [Pokédex](https://fr.wikipedia.org/wiki/Pok%C3%A9dex).
+
+Le site fera en sorte d'être le plus [responsive](https://fr.wikipedia.org/wiki/Site_web_r%C3%A9actif) possible selon la taille d'écran.
+
+Le JS que comprend le site est principalement utilisé pour l'affichage et l'animation de la [navbar](https://www.w3schools.com/css/css_navbar.asp) ainsi que les requêtes de PokéAPI.
+
